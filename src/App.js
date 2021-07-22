514f696d49bb7373a8800b26b2eacb89a73087c3
Josh Beatty <joshuabeatty88@gmail.com>
2021-07-21 23:42:26 -0600

    Setup text banner component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component, useEffect, useState} from "react";
+import {CardList} from './components/card-list/card-list.component'
+import {SearchBox} from "./components/search-box/search-box.component";
+import {TextBanner} from "./components/text_banner/text-banner.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*const App = () => {
+    const [monsters, setMonsters] = useState([])
+    const [searchInput, setSearchInput] = useState('')
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(resp => resp.json())
+            .then(users => setMonsters(users))
+    }, [])
+
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchInput.toLowerCase()))
+
+
+    return(
+        <div className={'App'}>
+            <input type={'search'} placeholder={'Search'} onChange={e => setSearchInput(e.target.value)} />
+            <CardList monsters={filteredMonsters} />
+        </div>
+    )
+}*/
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            monsters: [],
+            searchInput: ''
+        };
+
+        /* Must bind methods to set context of 'this' */
+        // this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(resp => resp.json())
+            .then(users => this.setState({monsters: users}))
+    }
+
+    /* Don't forget to BIND methods in the constructor or use arrow functions */
+    /*handleChange(e) {
+        this.setState({searchInput: e.target.value})
+    }*/
+
+    /* ES6: Automatically bound (lexical scoping) 'this' to place where method defined (in class)
+    * This means we no longer have to bind it manually with regular methods
+    *  */
+    handleChange = (e) => {
+        this.setState({searchInput: e.target.value})
+    }
+
+    render() {
+        const {monsters, searchInput} = this.state;
+        const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchInput.toLowerCase()))
+        return (
+            <div className="App">
+                <TextBanner>Monsters Rolodex</TextBanner>
+                <SearchBox placeholder={'Search Monsters'} handleChange={this.handleChange} />
+                <CardList monsters={filteredMonsters} />
+            </div>
+        );
+    }
 }
 
 export default App;
